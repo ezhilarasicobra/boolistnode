e97da60c60bd9a871e9db5090fed65534b350935
Ezhilarasi <tezzhilarasi@gmail.com>
2021-11-30 17:53:35 +0530

    updated DB

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  "mongodb+srv://ezhil:ezhil@cluster0.ix2r8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" || 'mongodb://localhost/googlebooks',
+  "mongodb+srv://ezhil:ezhil@cluster0.09fss.mongodb.net/BookSearch?retryWrites=true&w=majority",
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
