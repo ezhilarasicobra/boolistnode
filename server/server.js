0a8ea90726fbc03dc75bdf80c32c52d7b70bf8de
Ezhilarasi <tezzhilarasi@gmail.com>
2021-12-16 13:50:08 +0530

    updated server details

--- server/server.js
+++ server/server.js
@@ -43,8 +43,6 @@ app.use(routes); //comment this out in the end
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    // log where we can go to test our GQL API
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 
   });
 });
